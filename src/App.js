72f2976a58d6e2c763dab95b3cf432c6471f3edf
مینا قلی زاده <1093@ronix.ir>
2022-01-17 16:24:48 +0330

    add canvas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const html = document.documentElement;
+    const canvas = document.getElementById("hero-lightpass");
+    const context = canvas.getContext("2d");
+
+    const frameCount = 148;
+    const currentFrame = (index) =>
+      `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+        .toString()
+        .padStart(4, "0")}.jpg`;
+
+    const preloadImages = () => {
+      for (let i = 1; i < frameCount; i++) {
+        const img = new Image();
+        img.src = currentFrame(i);
+      }
+    };
+
+    const img = new Image();
+    img.src = currentFrame(1);
+    canvas.width = 1158;
+    canvas.height = 770;
+    img.onload = function () {
+      context.drawImage(img, 0, 0);
+    };
+
+    const updateImage = (index) => {
+      img.src = currentFrame(index);
+      context.drawImage(img, 0, 0);
+    };
+
+    window.addEventListener("scroll", () => {
+      const scrollTop = html.scrollTop;
+      const maxScrollTop = html.scrollHeight - window.innerHeight;
+      const scrollFraction = scrollTop / maxScrollTop;
+      const frameIndex = Math.min(
+        frameCount - 1,
+        Math.ceil(scrollFraction * frameCount)
+      );
+
+      requestAnimationFrame(() => updateImage(frameIndex + 1));
+    });
+
+    preloadImages();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="hero-lightpass"></canvas>
     </div>
   );
 }
